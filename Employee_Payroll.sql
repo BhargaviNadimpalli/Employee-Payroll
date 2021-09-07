59090d484710a4c30dc5bacb853619a9ee05acb4
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-07 10:26:31 -0600

    Feat: Added functions

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -41,3 +41,12 @@ update payroll set gender = 'M' WHere Name = 'Charlie' or Name = 'Bill'
 update payroll set Salary = 300000 WHere Name = 'Terisha'
 use Employee_Payroll
  select * from payroll 
+
+ select sum(salary) from payroll where gender='M' group by Gender;
+select sum(salary) from payroll  group by Gender;
+select sum(salary) as 'totalSalary',gender from payroll group by Gender;
+select avg(salary) as 'averageSalary',gender from payroll group by Gender;
+select min(salary) as 'minimumSalary',gender from payroll group by Gender;
+select max(salary) as 'maximumSalary',gender from payroll group by Gender;
+select count(Gender) as 'male' from payroll where gender='M' 
+select count(Gender) as 'female' from payroll where gender='F
