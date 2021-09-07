388099a2cbe9c512698eaaa51df8a56090d61393
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-07 09:53:38 -0600

    Feat: Created payroll table

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -5,3 +5,12 @@
 create Database Employee_Payroll
 select * from sys.databases
 use Employee_Payroll
+
+--UC2 Create Table employee_payroll in Payroll Service
+Create table payroll	(
+	Id int identity(1, 1) primary Key,
+	Name varchar(50),
+	Salary money Not Null,
+	Start datetime Not Null
+	)
+	select * from payroll
